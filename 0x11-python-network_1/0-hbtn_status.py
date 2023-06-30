f5df33a49a74fa8721066c5f63f5b7183534cdbe
Daniel Oladele <50686796+danieloladele7@users.noreply.github.com>
2023-06-30 23:23:08 +0000

    task 0 and 2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """A script that
 - fetches https://intranet.hbtn.io/status.
-- uses urlib package
-- must include with
+- uses urlib package.
+- must include with.
 """
 
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-        content = r.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
