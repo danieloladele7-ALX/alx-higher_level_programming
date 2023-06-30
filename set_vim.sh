3adcb3fd2a01b7b40904883c19e0d044c15ec06b
Daniel Oladele <50686796+danieloladele7@users.noreply.github.com>
2023-06-30 22:57:15 +0000

    set_vim added

---
+++ set_vim.sh
@@ -0,0 +1,12 @@
+#!/usr/bash
+
+# install neovim
+apt-get install neovim -y
+
+# make nvim config folder and copy init
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim
+
+# install vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
