6e782bdf1d482a297a91fc2893403981882d12ba
Daniel Oladele <danieloladele7@gmail.com>
2023-07-04 10:48:19 +0100

    web test for 0.0.0.0:5050/status set

---
+++ 0x11-python-network_1/web_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Web server
+"""
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Root
+    """
+    return "Index"
+
+@app.route("/status", strict_slashes=False)
+def status():
+    """ Status
+    """
+    return "Custom status"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
